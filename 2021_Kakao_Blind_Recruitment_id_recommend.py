408d7f52ce5359081dff12169c5b0ef9159b3af6
ORCT <sonic407@naver.com>
2021-09-09 18:13:41 +0900

    solve 1-2

--- 2021_Kakao_Blind_Recruitment_id_recommend.py
+++ 2021_Kakao_Blind_Recruitment_id_recommend.py
@@ -1,6 +1,8 @@
+import re
 def solution(new_id):
-    new_id = new_id.lower()
-    answer = new_id
+    new_id_1 = new_id.lower()
+    new_id_2 = re.sub('[~!@#$%^&*()=+{}:?,<>/]', '', new_id_1)
+    answer = new_id_2
     return answer
 
 id = solution(input())
